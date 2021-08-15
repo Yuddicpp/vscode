a00a8f7c3ec615d2e59cc1562e1291e98abe79b7
Yuddicpp <1652534962@qq.com>
2021-08-15 16:08:16 +0800

    hello

---
+++ cpp/hello.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+    printf("HelloWorld\n");
+    int a = 0;
+    scanf("%d",&a);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n",i);
+    }
+    printf("%d\n",a);
+    
+    return 0;
+}
