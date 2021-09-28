a0a01185c6b9ef8e9bc4075d2016a76ff1d45015
Yuddicpp <1652534962@qq.com>
2021-09-28 20:39:19 +0800

    python

--- python/hello.py
+++ python/hello.py
@@ -1,18 +1,9 @@
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import PoissonRegressor
-from sklearn.experimental import enable_hist_gradient_boosting  # noqa
-from sklearn.ensemble import HistGradientBoostingRegressor
+import torch 
+from torch import nn
 
-n_samples, n_features = 1000, 20
-rng = np.random.RandomState(0)
-X = rng.randn(n_samples, n_features)
-# positive integer target correlated with X[:, 5] with many zeros:
-y = rng.poisson(lam=np.exp(X[:, 5]) / 2)
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=rng)
-glm = PoissonRegressor()
-gbdt = HistGradientBoostingRegressor(loss='poisson', learning_rate=.01)
-glm.fit(X_train, y_train)
-gbdt.fit(X_train, y_train)
-print(glm.score(X_test, y_test))
-print(gbdt.score(X_test, y_test))
+print("torch version:", torch.__version__)
+
+a = torch.tensor([[2,1]])
+b = torch.tensor([[-1,2]])
+c = a@b.t()
+print("[[2,1]]@[[-1],[2]] =", c.item())
