cfde7b9357cc8f27776bb40e6d698bd0d2d422af
Yuddicpp <1652534962@qq.com>
2021-09-28 22:46:49 +0800

    pytorch

---
+++ python/pytorch/eat_pytorch_in_20_days/test.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import torch 
+from torch import nn 
+from torch.utils.data import Dataset,DataLoader,TensorDataset
+
+dftrain_raw = pd.read_csv('data/titanic/train.csv')
+dftest_raw = pd.read_csv('data/titanic/test.csv')
+dftrain_raw.head(10)
