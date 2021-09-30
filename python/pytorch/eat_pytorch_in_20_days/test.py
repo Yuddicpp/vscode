285c7b7cc32deae7a4117fd03f8ab0821fb9fe00
Yuddicpp <1652534962@qq.com>
2021-09-30 16:06:32 +0800

    data process

--- python/pytorch/eat_pytorch_in_20_days/test.py
+++ python/pytorch/eat_pytorch_in_20_days/test.py
@@ -1,6 +1,4 @@
 import os, sys
-
-from numpy.core.numeric import indices
 os.chdir(sys.path[0])
 
 import numpy as np
@@ -10,43 +8,31 @@ import torch
 from torch import nn 
 from torch.utils.data import Dataset,DataLoader,TensorDataset
 
+# Survived:0代表死亡，1代表存活【y标签】
+# Pclass:乘客所持票类，有三种值(1,2,3) 【转换成onehot编码】
+# Name:乘客姓名 【舍去】
+# Sex:乘客性别 【转换成bool特征】
+# Age:乘客年龄(有缺失) 【数值特征，添加“年龄是否缺失”作为辅助特征】
+# SibSp:乘客兄弟姐妹/配偶的个数(整数值) 【数值特征】
+# Parch:乘客父母/孩子的个数(整数值)【数值特征】
+# Ticket:票号(字符串)【舍去】
+# Fare:乘客所持票的价格(浮点数，0-500不等) 【数值特征】
+# Cabin:乘客所在船舱(有缺失) 【添加“所在船舱是否缺失”作为辅助特征】
+# Embarked:乘客登船港口:S、C、Q(有缺失)【转换成onehot编码，四维度 S,C,Q,nan】
 
-# Series
-# data = pd.Series(np.random.randn(5), index=["a", "b", "c", "d", "e"])
-# print(data)
-
-# Dataframe
-# d= {
-#     "one": pd.Series([1.0, 2.0, 3.0], index=["a", "b", "c"]),
-#     "two": pd.Series([1.0, 2.0, 3.0, 4.0], index=["a", "b", "c", "d"]),
-# }
-# data = pd.DataFrame(d)
-# print(data.columns)
-# df2 = pd.DataFrame(
-#     {
-#         "A": 1.0,
-#         "B": pd.Timestamp("20130102"),
-#         "C": pd.Series(1, index=list(range(4)), dtype="float32"),
-#         "D": np.array([3] * 4, dtype="int32"),
-#         "E": pd.Categorical(["test", "train", "test", "train"]),
-#         "F": "foo",
-#     }
-# )
-# print(df2.iloc[:,0:1])
 
 dftrain_raw = pd.read_csv('./data/titanic/train.csv')
 dftest_raw = pd.read_csv('./data/titanic/test.csv')
 
-
-print(dftrain_raw['Pclass'].index)
-print(pd.get_dummies(dftrain_raw['Pclass']))
+# Process data
 def preprocessing(dfdata):
-
     dfresult= pd.DataFrame()
 
     #Pclass
+    # get_dummies():Convert categorical variable into dummy/indicator variables.
     dfPclass = pd.get_dummies(dfdata['Pclass'])
     dfPclass.columns = ['Pclass_' +str(x) for x in dfPclass.columns ]
+    # concat():Concatenate pandas objects along a particular axis with optional set logic along the other axes.
     dfresult = pd.concat([dfresult,dfPclass],axis = 1)
 
     #Sex
@@ -54,6 +40,7 @@ def preprocessing(dfdata):
     dfresult = pd.concat([dfresult,dfSex],axis = 1)
 
     #Age
+    # fillna():Fill NA/NaN values using the specified method.
     dfresult['Age'] = dfdata['Age'].fillna(0)
     dfresult['Age_null'] = pd.isna(dfdata['Age']).astype('int32')
 
@@ -70,13 +57,14 @@ def preprocessing(dfdata):
     dfEmbarked.columns = ['Embarked_' + str(x) for x in dfEmbarked.columns]
     dfresult = pd.concat([dfresult,dfEmbarked],axis = 1)
 
-    return(dfresult)
+    return dfresult
 
-# x_train = preprocessing(dftrain_raw).values
-# y_train = dftrain_raw[['Survived']].values
 
-# x_test = preprocessing(dftest_raw).values
-# y_test = dftest_raw[['Survived']].values
+x_train = preprocessing(dftrain_raw).values
+y_train = dftrain_raw[['Survived']].values
+
+x_test = preprocessing(dftest_raw).values
+y_test = dftest_raw[['Survived']].values
 
 # print("x_train.shape =", x_train.shape )
 # print("x_test.shape =", x_test.shape )
@@ -84,5 +72,7 @@ def preprocessing(dfdata):
 # print("y_train.shape =", y_train.shape )
 # print("y_test.shape =", y_test.shape )
 
-# print(x_train)
+dl_train = DataLoader(TensorDataset(torch.tensor(x_train).float(),torch.tensor(y_train).float()),shuffle = True, batch_size = 8)
+dl_valid = DataLoader(TensorDataset(torch.tensor(x_test).float(),torch.tensor(y_test).float()),shuffle = False, batch_size = 8)
+
 
