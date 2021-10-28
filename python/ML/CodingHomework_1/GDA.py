69da5c89d1bd489354ecf62518d4ca736c661fef
Yuddicpp <1652534962@qq.com>
2021-10-28 10:22:24 +0800

    hw

--- python/ML/CodingHomework_1/GDA.py
+++ python/ML/CodingHomework_1/GDA.py
@@ -1,40 +1,90 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 
-# for dirname, _, filenames in os.walk('Dataset_traffic-driving-style-road-surface-condition'):
-#     for filename in filenames:
-#         print(os.path.join(dirname, filename))
+# 使用pandas读取实验数据
+df_opel_corsa_01 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/opel_corsa_01.csv',delimiter=';')
 
-df_opel_corsa_01 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/opel_corsa_01.csv',sep='.',delimiter=';')
+df_opel_corsa_02 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/opel_corsa_02.csv',delimiter=';')
 
-df_opel_corsa_02 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/opel_corsa_02.csv',sep='.',delimiter=';')
+df_peugeot_207_01 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/peugeot_207_01.csv',delimiter=';')
 
-df_peugeot_207_01 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/peugeot_207_01.csv',sep='.',delimiter=';')
+df_peugeot_207_02 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/peugeot_207_02.csv',delimiter=';')
 
-df_peugeot_207_02 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/peugeot_207_02.csv',sep='.',delimiter=';')
-
-
-# 使用skilearn
+# 实验数据处理，将‘AggressiveStyle’设为1，另一状态设为0
 def Road_surface_data_process(data_raw):
     roadSurface_to_state = {
         'EvenPaceStyle' : 0,
-        'AggressiveStyle.' : 1
+        'AggressiveStyle' : 1
     }
     y = data_raw['drivingStyle'].map(roadSurface_to_state).fillna(0).to_numpy()
-    x = data_raw.iloc[:,:14].fillna(1).to_numpy()
+    x = data_raw.iloc[:,:14].fillna(0).to_numpy()
     for i in range(x.shape[0]):
         for j in range(x.shape[1]):
             if type(x[i,j]) == str:
-                x[i,j] = float(x[i,j].replace(',','.'))
+                x[i,j] = float(x[i,j].replace(',','.'))  #由于实验数据中小数点为‘,’,更改为‘.’
     return x,y
 
 
 X,Y = Road_surface_data_process(pd.concat([df_opel_corsa_01, df_opel_corsa_02, df_peugeot_207_01, df_peugeot_207_02], axis=0))
+# 分为数据集和测试集
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
-clf = GaussianNB()
-clf.fit(x_train, y_train)
-print(accuracy_score(clf.predict(x_test),y_test))
+
+
+# 计算高斯模型的参数
+def train(x_train,y_train):
+
+    n = y_train.shape[0]
+    y_0=0
+    for i in range(n):
+        if y_train[i]==0:
+            y_0+=1
+    y_1=n-y_0
+    phi = y_1/n
+
+    mu_0 = np.zeros((14, 1))
+    for i in range(n):
+        if(y_train[i]==0):
+            mu_0 = mu_0 + np.expand_dims(x_train[i],axis=1)
+    mu_0 = mu_0/y_0
+
+    mu_1 = np.zeros((14, 1))
+    for i in range(n):
+        if(y_train[i]==1):
+            mu_1 = mu_1 + np.expand_dims(x_train[i],axis=1)
+    mu_1 = mu_1/y_1
+
+    cov = np.zeros((14, 14))   # convariance of two Gaussians
+    for x, y in zip(x_train, y_train):
+        x = np.expand_dims(x,1)
+        if y == 0:
+            cov = cov + np.matmul(x - mu_0, (x - mu_0).T)
+        else:
+            cov = cov + np.matmul(x - mu_1, (x - mu_1).T)
+    cov =  cov/n
+
+    return phi,mu_0.astype('float32'),mu_1.astype('float32'),cov.astype('float32')
+
+#计算高斯模型下的概率值
+def compute_p(x,mu_0,mu_1,cov,phi):
+    p0 = np.exp((-0.5*np.dot(np.dot((np.transpose(x-mu_0)),cov),(x-mu_0)))[0,0])*(1-phi)
+    p1 = np.exp((-0.5*np.dot(np.dot((np.transpose(x-mu_1)),cov),(x-mu_1)))[0,0])*phi
+    if(p0>p1):
+        return 0
+    else:
+        return 1
+
+
+#测试函数
+def predict(x_test,y_test):
+    phi,mu_0,mu_1,cov = train(x_train,y_train)
+    y_pre = []
+    for i in range(x_test.shape[0]):
+        y_pre.append(compute_p(np.expand_dims(x_test[i],1),mu_0,mu_1,np.matrix(cov).I,phi))
+    
+    print(accuracy_score(y_pre,y_test))
+
+
+predict(x_test,y_test)
+
--- python/ML/CodingHomework_1/logistic_pytorch.py
+++ python/ML/CodingHomework_1/logistic_pytorch.py
@@ -10,18 +10,16 @@ from torch.utils.data import Dataset,DataLoader,TensorDataset
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
-# for dirname, _, filenames in os.walk('Dataset_traffic-driving-style-road-surface-condition'):
-#     for filename in filenames:
-#         print(os.path.join(dirname, filename))
+# 使用pandas读取实验数据
+df_opel_corsa_01 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/opel_corsa_01.csv',delimiter=';')
 
-df_opel_corsa_01 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/opel_corsa_01.csv',sep='.',delimiter=';')
+df_opel_corsa_02 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/opel_corsa_02.csv',delimiter=';')
 
-df_opel_corsa_02 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/opel_corsa_02.csv',sep='.',delimiter=';')
+df_peugeot_207_01 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/peugeot_207_01.csv',delimiter=';')
 
-df_peugeot_207_01 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/peugeot_207_01.csv',sep='.',delimiter=';')
+df_peugeot_207_02 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/peugeot_207_02.csv',delimiter=';')
 
-df_peugeot_207_02 = pd.read_csv('Dataset_traffic-driving-style-road-surface-condition/peugeot_207_02.csv',sep='.',delimiter=';')
-
+# 实验数据处理，将结果划分为三维向量
 def Road_surface_data_process(data_raw):
     y = pd.get_dummies(data_raw['roadSurface']).to_numpy()
     x = data_raw.iloc[:,:14].fillna(0).to_numpy()
@@ -35,44 +33,50 @@ def Road_surface_data_process(data_raw):
 X,Y = Road_surface_data_process(pd.concat([df_opel_corsa_01, df_opel_corsa_02, df_peugeot_207_01, df_peugeot_207_02], axis=0))
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
 
+#归一化
 min_max_scaler = preprocessing.MinMaxScaler()
 x_scaled = min_max_scaler.fit(x_train)
-
 x_train_transformed = min_max_scaler.transform(x_train)
 x_test_transformed = min_max_scaler.transform(x_test)
 
+# GPU训练
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 dl_train = DataLoader(TensorDataset(torch.tensor(x_train_transformed).float().to(device),torch.tensor(y_train).float().to(device)),shuffle = True, batch_size = 8)
 dl_valid = DataLoader(TensorDataset(torch.tensor(x_test_transformed).float().to(device),torch.tensor(y_test).float().to(device)),shuffle = False, batch_size = 8)
 
-
+# 创建网络
 def create_net():
     net = nn.Sequential()
-    net.add_module("linear1",nn.Linear(14,40))
+    net.add_module("linear1",nn.Linear(14,20))
     net.add_module("relu1",nn.ReLU())
-    net.add_module("linear2",nn.Linear(40,20))
+    net.add_module("linear2",nn.Linear(20,30))
     net.add_module("relu2",nn.ReLU())
-    net.add_module("linear3",nn.Linear(20,6))
+    net.add_module("linear3",nn.Linear(30,15))
     net.add_module("relu3",nn.ReLU())
-    net.add_module("linear4",nn.Linear(6,3))
+    net.add_module("linear4",nn.Linear(15,6))
+    net.add_module("relu4",nn.ReLU())
+    net.add_module("linear5",nn.Linear(6,3))
     net.add_module("sigmoid",nn.Sigmoid())
     return net
     
 net = create_net()
 
-# 定义模型
-... 
 net = net.to(device) # 移动模型到cuda
 
 # 训练模型
 
-net.load_state_dict(torch.load('logistic.pkl'))
+# net.load_state_dict(torch.load('logistic.pkl'))
 
+# 损失和优化函数定义
 loss_func  = torch.nn.BCELoss()
 optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
 acc = []
+
+# 迭代次数
 EPOCHS = 1000
+
+#开始训练
 for epoch in range(1,EPOCHS):  
 
     net.train()
@@ -123,7 +127,10 @@ torch.save(net.state_dict(), "./logistic.pkl")
 
 plt.plot(range(1,EPOCHS),acc)
 plt.ylim(0, 1);
+plt.xlabel("EPOCH")
+plt.ylabel("Accuracy")
 plt.show()
 
 # Result:
-# loss = 0.026,Accuracy  = 0.983, val_loss = 0.063, val_Accuracy = 0.971
+
+#EPOCH = 999, loss = 0.026,Accuracy  = 0.983, val_loss = 0.053, val_Accuracy = 0.976
