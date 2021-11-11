48989310cab81a62073906b05e9aa317eb5269f8
Yuddicpp <1652534962@qq.com>
2021-11-11 16:55:50 +0800

    hw2

---
+++ python/big_data/HW2/data_process.py
@@ -0,0 +1,22 @@
+from numpy.core.numeric import NaN
+import pandas as pd
+import numpy as np
+# df_train = pd.read_csv('Project2-data/netflix_train.txt',sep = ' ',names=['users','movie_title','rank','data'])
+# df_users = pd.read_csv('Project2-data/users.txt',names = ['users'])
+
+# data_train = pd.DataFrame(0,index = np.array(df_users['users']),columns=np.array(range(1,10001)))
+
+
+# for index, row in df_train.iterrows():
+#     print(index,row['rank'])
+#     data_train.loc[row['users'],row['movie_title']] = row['rank']
+
+# print(data_train)
+# data_train.to_csv("data_train.csv")
+
+
+df_train = pd.read_csv('data_train.csv',index_col=0)
+print(df_train)
+
+# df_test = pd.read_csv('data_test.csv',index_col=0)
+# print(df_test)
