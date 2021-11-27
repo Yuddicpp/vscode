e537d538c2266f9cc48d6f84c5637ce7993ef1d1
Yuddicpp <1652534962@qq.com>
2021-11-27 11:49:57 +0800

    leetcode

--- python/leetcode/15. 三数之和.py
+++ python/leetcode/15. 三数之和.py
@@ -5,32 +5,33 @@ nums = [82597,-9243,62390,83030,-97960,-26521,-61011,83390,-38677,12333,75987,46
 
 
 def threeSum(nums: List[int]) -> List[List[int]] :
+    n = len(nums)
     nums.sort()
-    # print(nums)
-    lenth = len(nums)
-    l = 0
-    ans = []
-    while(l<lenth-2):
-        i_nums = []
-        for i in range(l+1,lenth-1):
-            # print('i:')
-            # print(i,i_nums)
-            if(i_nums.count(nums[i])==0):
-                i_nums.append(nums[i])
-                j_nums = []
-                for j in range(i+1,lenth):
-                    # print("j:")
-                    # print(j,j_nums)
-                    if(j_nums.count(nums[j])==0):
-                        j_nums.append(nums[j])
-                        # print(j_nums)
-                        if (nums[l]+nums[i]+nums[j])==0:
-                            ans.append([nums[l],nums[i],nums[j]])
-                            # print("ans:")
-                            # print(ans)
-        l+=1
-        while(nums[l]==nums[l-1] and l<lenth-1):
-            l+=1
+    ans = list()
+    
+    # 枚举 a
+    for first in range(n):
+        # 需要和上一次枚举的数不相同
+        if first > 0 and nums[first] == nums[first - 1]:
+            continue
+        # c 对应的指针初始指向数组的最右端
+        third = n - 1
+        target = -nums[first]
+        # 枚举 b
+        for second in range(first + 1, n):
+            # 需要和上一次枚举的数不相同
+            if second > first + 1 and nums[second] == nums[second - 1]:
+                continue
+            # 需要保证 b 的指针在 c 的指针的左侧
+            while second < third and nums[second] + nums[third] > target:
+                third -= 1
+            # 如果指针重合，随着 b 后续的增加
+            # 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+            if second == third:
+                break
+            if nums[second] + nums[third] == target:
+                ans.append([nums[first], nums[second], nums[third]])
+    
     return ans
 
 
