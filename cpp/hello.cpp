e28b42c606798ba0708fa7330d06134e9a300d8d
Yuddicpp <1652534962@qq.com>
2021-11-27 20:14:08 +0800

    cpp

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -8,9 +8,27 @@
 using namespace std;
 
 
-int main(){
-    stack<int> s;
+int fib(int n) {
+    if(n==0) return 0;
+    else if(n==1) return 1;
+    else{
+        long long int x=0;
+        long long int y=1;
+        long long int result = x+y;
+        n=n-1;
+        while(n>0){
+            result = x + y;
+            result = result%1000000007;
+            x = y;
+            y = result;
+            n--;
+        }
+        return result;
+    }
+}
 
+int main(){
+    cout<<fib(95)<<endl;
     
 
     return 0;
