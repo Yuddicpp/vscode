ccc3c6e4b849ee296b63765ff36c7c9b611c0a70
Yuddicpp <1652534962@qq.com>
2022-03-26 20:39:04 +0800

    settings

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -8,31 +8,14 @@
 
 using namespace std;
 
-
-// int fib(int n) {
-//     if(n==0) return 0;
-//     else if(n==1) return 1;
-//     else{
-//         long long int x=0;
-//         long long int y=1;
-//         long long int result = x+y;
-//         n=n-1;
-//         while(n>0){
-//             result = x + y;
-//             result = result%1000000007;
-//             x = y;
-//             y = result;
-//             n--;
-//         }
-//         return result;
-//     }
-// }
-
 int main(){
-    // cout<<fib(95)<<endl;
-    int k[4]={1,2,3,4};
-    int *j=&k[0];
-    cout<<j<<endl;
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        cout<<"Hello World"<<endl;
+    }
+    
+    
     return 0;
 }
 
