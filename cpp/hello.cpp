59a9c4746a75392360fda726f1d04ec37b7d974a
Yuddicpp <1652534962@qq.com>
2022-03-27 11:53:09 +0800

    cpp_learning

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,21 +1,17 @@
-#include <stdio.h>
-
-
-#include<iostream>
-#include<stack>
-#include<queue>
-#include<vector>
-
-using namespace std;
+// 使用cin/cout必须包含iostream文件以及std命名空间
+#include <iostream>
 
 int main(){
-    for (int i = 0; i < 5; i++)
+    using namespace std;
+    for (int i = 0; i < 3; i++)
     {
         /* code */
+        // 尽量在首次使用变量前声明它
+        int* variable;
+        cout<<&variable<<endl;
         cout<<"Hello World"<<endl;
     }
-    
-    
+    cout<<cin.get();
     return 0;
 }
 
